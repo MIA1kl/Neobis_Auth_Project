54c8b59b1a1a0a8c394c1e7ad5f28c836244be54
MIA1kl <mia01kln@gmail.com>
2023-05-29 17:11:50 +0600

    Added fields to User model

--- authentication/models.py
+++ authentication/models.py
@@ -29,8 +29,11 @@ class UserManager(BaseUserManager):
         return user
     
 class User(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField(max_length=255, unique=True, db_index = True)
     email = models.EmailField(max_length=255, unique=True, db_index=True)
+    username = models.CharField(max_length=255, unique=True, db_index = True)
+    first_name = models.CharField(max_length=255, null=True, blank=True)
+    last_name = models.CharField(max_length=255, null=True, blank=True)
+    birth_date = models.DateField(null=True, blank=True, default=None)
     is_verified = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
